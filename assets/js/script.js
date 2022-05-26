bf98756ac13bfde84dfdbeb6840bed8638f52514
SANTHOSH SIVAN A <87321978+LearnAtExperience@users.noreply.github.com>
2022-05-26 18:00:41 +0530

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.navbarToggler').onclick = () => {
-  document.querySelector('.line1').classList.toggle('anim1');
-  document.querySelector('.line2').classList.toggle('anim2');
-  document.querySelector('.line3').classList.toggle('anim3');
-  document.querySelector('.navMenu').classList.toggle('anim4');
-}
